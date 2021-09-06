a2852a3fe22cd0bf40ed9542efbc5ead929afa1c
Chanyx666 <christianresma8@gmail.com>
2021-09-06 16:31:34 +0800

    update comment

--- Market Application/SplashPage.cs
+++ Market Application/SplashPage.cs
@@ -36,7 +36,7 @@ namespace Market_Application
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-
+            //on appear app animate splash screen
             await splashImage.ScaleTo(1, 2000);
             await splashImage.ScaleTo(0.9, 1500, Easing.Linear);
             await splashImage.ScaleTo(150, 1200, Easing.Linear);
